2c28d0f0fc7e6ef523747eb8ee40042d290d9aae
Pavan Kalyan <127199710+PavanUmmagani07@users.noreply.github.com>
2024-05-31 17:15:23 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -19,3 +19,22 @@ function fizzbuzz() {
 }
 
 fizzbuzz();
+
+
+#FIZZBUZZ USING FOR LOOP
+var output = [];
+function fizzbuzz() {
+  for (var count = 1; count <= 100; count++) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+  }
+  console.log(output);
+}
+fizzbuzz();
